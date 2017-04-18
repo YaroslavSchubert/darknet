4d3bd0c4e53acc12e36898f85df851cfda93a647
Yaroslav Schubert <YaroslavShubert@gmail.com>
2017-04-18 17:18:50 +0300

    add plot avg loss

---
+++ scripts/plot_loss.py
@@ -0,0 +1,48 @@
+# I am plotting the training log using the following script,
+# it requires matplotlib to be installed
+import argparse
+import sys
+import matplotlib.pyplot as plt
+
+
+def main(argv):
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "log_file",
+        help="path to log file"
+    )
+
+    parser.add_argument(
+        "option",
+        help="0 -> loss vs iter"
+    )
+
+    args = parser.parse_args()
+
+    f = open(args.log_file)
+
+    lines = [line.rstrip("\n") for line in f.readlines()]
+
+    # skip the first 3 lines
+    lines = lines[3:]
+
+    numbers = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
+
+    iters = []
+    loss = []
+    for line in lines:
+        if line[0] in numbers:
+            line_args = line.split(" ")
+            iters.append(int(line_args[0][:-1]))
+            loss.append(float(line_args[2]))
+    plt.plot(iters, loss)
+    plt.xlabel('iters')
+    plt.ylabel('avg loss')
+    plt.grid()
+
+    plt.show()
+
+if __name__ == "__main__":
+    main(sys.argv)
